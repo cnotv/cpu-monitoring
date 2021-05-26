75c7a941093b5ace0da375bc64c3b71de3fbfafc
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-05-26 22:28:28 +0200

    Configure server with API for CPU values

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+app.get('/api/cpu', (req, res) => {
+  const cpus = os.cpus().length
+  const loadAverage = os.loadavg()[0] / cpus
+
+  res.status(200).json({
+    average: loadAverage
+  });
+})
+
+app.listen(port, 'localhost', () => {
+  console.log(`Check CPU average at http://localhost:${port}/api/cpu`)
+})
